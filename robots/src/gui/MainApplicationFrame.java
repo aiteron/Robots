e09da0e529207f34213fe5bbfd5c3a507347af27
Blinov Alexander <sashablinov1998@gmail.com>
2020-05-19 21:21:52 +0500

    Fix config path error bug

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -11,7 +11,7 @@ public class MainApplicationFrame extends JFrame {
     private final Config config;
 
     public MainApplicationFrame() {
-        config = new Config("resources/config.properties",
+        config = new Config("src/resources/config.properties",
                 "resources/LocalizationResources");
 
         int inset = 50;
